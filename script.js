efaf852729913154a53c8542041154acdc419229
GustaParente <gustavoparentedev@gmail.com>
2023-10-31 18:42:46 -0300

    Mais e mais mudanças

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let ultimoIndice = -1;
 
 function obterFraseAutor() {
@@ -78,3 +79,40 @@ function obterFraseAutor() {
   const botao = document.getElementById('botao');
   botao.addEventListener('click', obterFraseAleatoria());
   obterFraseAutor();
+
+        var txtInput = document.querySelector('#frase');
+        var voiceList = document.querySelector('#voiceList');
+        var btnSpeak = document.querySelector('#btnSpeak');
+        var synth = window.speechSynthesis;
+        var voices = [];
+
+        PopulateVoices();
+        if(speechSynthesis !== undefined){
+            speechSynthesis.onvoiceschanged = PopulateVoices;
+        }
+
+        btnSpeak.addEventListener('click', ()=> {
+            var toSpeak = new SpeechSynthesisUtterance(txtInput.textContent);
+            var selectedVoiceName = voiceList.selectedOptions[0].getAttribute('data-name');
+            voices.forEach((voice)=>{
+                if(voice.name === selectedVoiceName){
+                    toSpeak.voice = voice;
+                }
+            });
+            synth.speak(toSpeak);
+        });
+
+        function PopulateVoices(){
+            voices = synth.getVoices();
+            var selectedIndex = voiceList.selectedIndex < 0 ? 0 : voiceList.selectedIndex;
+            voiceList.innerHTML = '';
+            voices.forEach((voice)=>{
+                var listItem = document.createElement('option');
+                listItem.textContent = voice.name;
+                listItem.setAttribute('data-lang', voice.lang);
+                listItem.setAttribute('data-name', voice.name);
+                voiceList.appendChild(listItem);
+            });
+
+            voiceList.selectedIndex = selectedIndex;
+        }
