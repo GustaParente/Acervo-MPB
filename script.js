957154d789fcaf45253ccd7cadad8304756c398d
GustaParente <gustavoparentedev@gmail.com>
2023-10-31 12:09:53 -0300

    Update maximo

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 let ultimoIndice = -1;
 
-function obterFraseAleatoria() {
+function obterFraseAutor() {
+  const seletor = document.getElementById('seletor');
+  const autor = seletor.value;
     fetch('frases.json')
       .then(response => response.json())
       .then(data => {
-        let novoIndice;
-        do {
-          novoIndice = Math.floor(Math.random() * data.length);
-        } while (novoIndice === ultimoIndice);
+        let dadosFiltrados = data.filter(item => item.autor === autor);
+        let novoIndice = dadosFiltrados;
+        novoIndice = Math.floor(Math.random() * dadosFiltrados.length);
+        
         
         // Armazene o novo índice como o último índice
         ultimoIndice = novoIndice;
 
         // Use a frase aleatória com o índice gerado
-        const fraseAleatoria = data[novoIndice];
+        const fraseAleatoria = dadosFiltrados[novoIndice];
   
         // Exibir a frase na página
         const fraseElement = document.getElementById('frase');
@@ -36,6 +38,43 @@ function obterFraseAleatoria() {
       });
   }
 
+  function obterFraseAleatoria() {
+      fetch('frases.json')
+        .then(response => response.json())
+        .then(data => {
+          let novoIndice;
+          do {
+            novoIndice = Math.floor(Math.random() * data.length);
+          } while (novoIndice === ultimoIndice);
+          
+          // Armazene o novo índice como o último índice
+          ultimoIndice = novoIndice;
+  
+          // Use a frase aleatória com o índice gerado
+          const fraseAleatoria = data[novoIndice];
+    
+          // Exibir a frase na página
+          const fraseElement = document.getElementById('frase');
+          const fraseAutorAno = document.getElementById('autor');
+          const fraseDesc = document.getElementById('desc');
+          
+          fraseElement.textContent = `"${fraseAleatoria.frase}"`;
+          fraseAutorAno.textContent = `${fraseAleatoria.autor}, ${fraseAleatoria.ano}`;
+          fraseDesc.textContent = `${fraseAleatoria.desc}`;
+  
+          const fraseVideo = document.getElementById('videoMusica');
+          fraseVideo.src = fraseAleatoria.link;
+  
+          const linkMusicaElement = document.getElementById('linkMusica');
+          linkMusicaElement.href = fraseAleatoria.linkYoutube;
+          linkMusicaElement.style.display = 'block';        
+        })
+        .catch(error => {
+          console.error('Erro ao obter frases: ', error);
+        });
+    }
+
   // Adicionar um evento de clique ao botão
   const botao = document.getElementById('botao');
-  botao.addEventListener('click', obterFraseAleatoria);
+  botao.addEventListener('click', obterFraseAleatoria());
+  obterFraseAutor();
